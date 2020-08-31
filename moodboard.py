d5e2008f7d9e07f6fc6b3e3f3c9553b8f2407f8a
JonahTuckman <jtuckman9@gmail.com>
2020-08-31 11:57:10 -0400

    First sorting function and writing function

---
+++ moodboard.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+
+dataset = pd.read_csv("MoodBoardData.csv")
+
+
+def sort_color(color):
+    color_data = pd.DataFrame(columns = ["Color","ImageLocation"])
+    count = 0
+    for index, row in dataset.iterrows():
+        if row['Color'] == color:
+            color_data.loc[count] = [row['Color']] + [row['ImageLocation']]
+            count = count + 1
+
+    return color_data
+
+## def sort_lightdark(shade):
+
+## def sort_room(room):
+
+
+
+def write_csvs_with_specs(color):
+    final_data = pd.DataFrame(columns = ['ImageLocation'])
+
+    # Color
+    color_data = sort_color(color)
+    final_data = pd.concat([color_data,final_data],join='inner')
+
+    # Shade
+
+    # Room
+
+    final_data.to_csv("final_data.csv")
+
+
+
+write_csvs_with_specs("White")
