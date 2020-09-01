393de84621f52e18b1468183c859900c9d899393
JonahTuckman <jtuckman9@gmail.com>
2020-09-01 13:28:03 -0400

    Merge

--- moodboard_optimization.py
+++ moodboard_optimization.py
@@ -11,9 +11,8 @@ def sort(column, name):
     count = 0
     for index, row in dataset.iterrows():
         if row[name].lower() == column:
-            color_data.loc[count] = [row['ImageLocation']
-            count = count + 1
-
+            color_data.loc[count] = row['ImageLocation']
+            count += 1
     return data
 
 def write_csvs_with_specs(color, cname, shade, sname, room, rname):
